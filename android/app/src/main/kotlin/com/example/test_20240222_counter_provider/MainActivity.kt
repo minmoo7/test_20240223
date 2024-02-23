8b5d5973a062fd0cf128457352d8f05bd0f46cfb
藤井大貴 <fujiidaiki@fujiidaikinoMacBook-Pro.local>
2024-02-23 23:29:38 +0900

    second commit

---
+++ android/app/src/main/kotlin/com/example/test_20240222_counter_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_20240222_counter_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'count_model.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => CountModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('counter'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Builder(builder: (context) {
+                // final model = Provider.of<CountModel>(context);
+                final model = context.watch<CountModel>();
+                return Text(
+                  '${model.counter}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              }),
+            ],
+          ),
+        ),
+        floatingActionButton: Consumer<CountModel>(builder: (context, model, child) {
+            return FloatingActionButton(
+              onPressed: () {
+                model.incrementCounter();
+              },
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            );
+          }
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
